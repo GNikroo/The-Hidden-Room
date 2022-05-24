450962c7379367f47a5ea9ec9107e833b982ba29
GNikroo <gina.nikroo@gmail.com>
2022-05-24 12:37:01 +0000

    Add Choice and Life classes. Refactor code

--- run.py
+++ run.py
@@ -1,4 +1,65 @@
+"""Import time to add delay"""
 import time
+
+
+class Choice:
+    """
+    Choice class.
+    """
+    def __init__(self, first_choice, second_choice):
+        self.first_choice = first_choice
+        self.second_choice = second_choice
+
+
+class Life:
+    """
+    Life class.
+    """
+    life = 3
+
+    @staticmethod
+    def reduce_life():
+        """
+        Reduce life by one.
+        """
+        Life.life -= 1
+        if Life.life > 0:
+            print("\033[1;30;41mOnly " + str(Life.life) + " lives.\n\033[m")
+        else:
+            time.sleep(1)
+            game_over()
+
+
+def read_script(lines):
+    """
+    Read specified lines from dungeon file.
+    """
+    time.sleep(1)
+    file = open("dungeon.txt", encoding="utf8")
+    for i, spec_line in enumerate(file):
+        if i in lines:
+            print(spec_line)
+    file.close()
+    time.sleep(1)
+
+
+def question_template(choice):
+    """
+    Offer choices for player.
+    """
+    while True:
+        user_input = input("Enter your choice: \n")
+        if user_input.lower() == choice.first_choice:
+            time.sleep(1)
+            print(f"You chose {user_input.lower()}." + "\n")
+            return choice.first_choice
+        elif user_input.lower() == choice.second_choice:
+            time.sleep(1)
+            print(f"You chose {user_input.lower()}." + "\n")
+            return choice.second_choice
+        else:
+            time.sleep(.5)
+            print("Something howls in the distance.\n")
 
 
 def game_logo():
@@ -12,369 +73,264 @@ def game_logo():
     time.sleep(1)
 
 
-def initialize_game():
+def scene1():
     """
     Begin game and present first question.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    print("Do you\033[1;32m investigate\033[m the corner or take to the\033[1;33m corridor\033[m?")
-    first_question()
+    print("\033[1;30;41mYou have " + str(Life.life) + " lives.\n\033[m")
+    lines = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    read_script(lines)
+    time.sleep(1)
+    print("Do you\033[1;31m investigate\033[m the corner\n")
+    print("or take to the\033[1;33m corridor\033[m?\n")
+    scene1_choice()
 
 
-def first_question():
+def scene1_choice():
     """
     Runs first question:
     Investigate corner or corridor
     """
-    user_input = "invalid"
-    while user_input == "invalid":
-        choice1 = input("Enter your choice: \n")
-        if choice1 == "corridor":
-            time.sleep(1)
-            print("You choose to take the\033[1;33m corridor\033[m\n")
-            user_input = "valid"
-            first_corridor()
-        elif choice1 == "investigate":
-            time.sleep(1)
-            print("You choose to\033[1;32m investigate\033[m the corner.\n")
-            user_input = "valid"
-            first_investigate()
-        else:
-            print("Something howls in the distance.\nPlease choose\033[1;32m investigate\033[m or take the\033[1;33m corridor\033[m.\n")
+    choice = Choice("corridor", "investigate")
+    user_input = question_template(choice)
+    if user_input == choice.first_choice:
+        corridor()
+    else:
+        scene1_investigate()
 
 
-def first_corridor():
+def corridor():
     """
     Reads the dungeon script for the corridor.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [11, 12, 13]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    game_over()
+    lines = [11, 12, 13]
+    read_script(lines)
+    time.sleep(1)
+    Life.reduce_life()
+    print("Do you\033[1;31m investigate\033[m the corner\n")
+    print("or take to the\033[1;33m corridor\033[m?\n")
+    scene1_choice()
 
 
-def first_investigate():
+def scene1_investigate():
     """
     Reads the dungeon script for the corner.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [16, 17, 18, 19, 20, 21, 22, 23, 24]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    print("Do you\033[1;35m walk\033[m towards the other side of the room or keep to the\033[1;36m walls\033[m?")
-    second_question()
+    lines = [16, 17, 18, 19, 20, 21, 22, 23, 24]
+    read_script(lines)
+    time.sleep(1)
+    print("Do you\033[1;32m walk\033[m towards the other side of the room\n")
+    print("or keep to the\033[1;34m walls\033[m?\n")
+    scene2()
 
 
-def second_question():
+def scene2():
     """
     Runs second question:
     Keep walking or hug the walls.
     """
-    user_input = "invalid"
-    while user_input == "invalid":
-        choice2 = input("Enter your choice: \n")
-        if choice2 == "walls":
-            time.sleep(1)
-            print("You choose to keep to the\033[1;36m walls\033[m.\n")
-            user_input = "valid"
-            second_walls()
-        elif choice2 == "walk":
-            time.sleep(1)
-            print("You choose to\033[1;35m walk\033[m to the other side.\n")
-            user_input = "valid"
-            second_walk()
-        else:
-            print("Small stones fall from the ceiling up ahead.\nPlease choose to\033[1;35m walk\033[m or hug the\033[1;36m walls\033[m.\n")
+    choice = Choice("walls", "walk")
+    user_input = question_template(choice)
+    if user_input == choice.first_choice:
+        scene3()
+    else:
+        walk()
 
 
-def second_walls():
+def scene3():
     """
     Reads the dungeon script for hugging the walls.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [27, 28, 29, 30, 31, 32, 33]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    print("Do you\033[1;31m investigate\033[m the opening or\033[1;34m continue\033[m along the perimeter?")
-    walls_question_1()
+    lines = [27, 28, 29, 30, 31, 32, 33]
+    read_script(lines)
+    time.sleep(1)
+    print("Do you\033[1;35m investigate\033[m the opening\n")
+    print("or\033[1;31m continue\033[m along the perimeter?\n")
+    scene3_choice()
 
 
-def walls_question_1():
+def scene3_choice():
     """
     Runs walls follow up question:
     Investigate the opening or continue.
     """
-    user_input = "invalid"
-    while user_input == "invalid":
-        choice2 = input("Enter your choice: \n")
-        if choice2 == "investigate":
-            time.sleep(1)
-            print("You choose to\033[1;31m investigate\033[m the opening.\n")
-            user_input = "valid"
-            walls_investigate()
-        elif choice2 == "continue":
-            time.sleep(1)
-            print("You choose to\033[1;34m continue\033[m to the other side.\n")
-            user_input = "valid"
-            walls_continue()
-        else:
-            print("Something bangs in the distance.\nPlease choose to\033[1;31m investigate\033[m or \033[1;34m continue\033[m.\n")
+    choice = Choice("investigate", "continue")
+    user_input = question_template(choice)
+    if user_input == choice.first_choice:
+        scene3_investigate()
+    else:
+        scene3_continue()
 
 
-def walls_investigate():
+def scene3_investigate():
     """
     Reads the dungeon script for investigating the opening.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [36, 37, 38, 39]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    print("Do you\033[1;33m approach\033[m the figure or\033[1;35m leave\033[m?")
-    walls_question_2()
+    lines = [36, 37, 38, 39]
+    read_script(lines)
+    time.sleep(1)
+    print("Do you\033[1;33m approach\033[m the figure\n")
+    print("or\033[1;32m leave\033[m?\n")
+    scene4()
 
 
-def walls_question_2():
+def scene4():
     """
     Runs wall investigation follow up question:
     Approach the figure or leave.
     """
-    user_input = "invalid"
-    while user_input == "invalid":
-        choice2 = input("Enter your choice: \n")
-        if choice2 == "approach":
-            time.sleep(1)
-            print("You choose to\033[1;31m approach\033[m the figure.\n")
-            user_input = "valid"
-            walls_investigate()
-        elif choice2 == "leave":
-            time.sleep(1)
-            print("You choose to\033[1;34m leave\033[m.\n")
-            user_input = "valid"
-            walls_continue()
-        else:
-            print("The sound of shuffling footsteps echo nearby.\nPlease choose to\033[1;31m approach\033[m or \033[1;34m leave\033[m.\n")
+    choice = Choice("approach", "leave")
+    user_input = question_template(choice)
+    if user_input == choice.first_choice:
+        approach()
+    else:
+        leave()
 
 
-def walls_approach():
+def approach():
     """
     Reads the dungeon script for approaching the figure.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [42, 43, 44, 45, 46, 47, 48]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [50, 51, 52, 53]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    sarcophagus_yes_compass()
+    lines = [42, 43, 44, 45, 46, 47, 48]
+    read_script(lines)
+    time.sleep(1)
+    lines = [50, 51, 52, 53]
+    read_script(lines)
+    time.sleep(1)
+    scene5()
 
 
-def walls_leave():
+def leave():
     """
     Reads the dungeon script for leaving the figure alone.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [56, 57, 58, 59, 60, 61]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    sarcophagus_no_compass()
+    lines = [56, 57, 58, 59, 60, 61]
+    read_script(lines)
+    time.sleep(1)
+    scene6()
 
 
-def walls_continue():
+def scene3_continue():
     """
     Reads the dungeon script for continuing to the perimeter.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [50, 51, 52, 53]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [70, 71, 72]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [75, 76, 77, 78, 79, 80]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()     
-    time.sleep(2)
-    game_over()
+    lines = [50, 51, 52, 53]
+    read_script(lines)
+    time.sleep(1)
+    lines = [70, 71, 72]
+    read_script(lines)
+    time.sleep(1)
+    lines = [75, 76, 77, 78, 79, 80]
+    read_script(lines)
+    time.sleep(1)
+    Life.reduce_life()
+    print("Do you\033[1;35m investigate\033[m the opening\n")
+    print("or\033[1;31m continue\033[m along the perimeter?\n")
+    scene3_choice()
 
 
-def second_walk():
+def walk():
     """
     Reads the dungeon script for walking to the other side.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [64, 65, 66, 67]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [70, 71, 72, 73]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    sarcophagus_no_compass()
+    lines = [64, 65, 66, 67]
+    read_script(lines)
+    time.sleep(1)
+    lines = [70, 71, 72, 73]
+    read_script(lines)
+    time.sleep(1)
+    scene6()
 
 
-def sarcophagus_no_compass():
+def scene6():
     """
     Reads the dungeon script to approach the sarcophagus
     without a compass.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [75, 76, 77, 78, 79, 80]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    game_over()
+    lines = [75, 76, 77, 78, 79, 80]
+    read_script(lines)
+    time.sleep(1)
+    Life.reduce_life()
+    print("Do you\033[1;32m walk\033[m towards the other side of the room\n")
+    print("or keep to the\033[1;34m walls\033[m?\n")
+    scene2()
 
 
-def sarcophagus_yes_compass():
+def scene5():
     """
     Reads the dungeon script to approach the sarcophagus
     with the compass.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [83, 84, 85, 86, 87]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    print("Do you give in to temptation and\033[1;31m look\033[m at what has emerged from the sarcophagus or do you\033[1;34m escape\033[m on?")
-    sarcophagus_question()
+    lines = [83, 84, 85, 86, 87]
+    read_script(lines)
+    time.sleep(1)
+    print("Do you give in to temptation and\033[1;34m look\033[m\n")
+    print("or do you\033[1;35m escape\033[m?\n")
+    scene5_choice()
 
 
-def sarcophagus_question():
+def scene5_choice():
     """
     Runs sarcophagus question:
     Look at what emerges or escape.
     """
-    user_input = "invalid"
-    while user_input == "invalid":
-        choice2 = input("Enter your choice: \n")
-        if choice2 == "look":
-            time.sleep(1)
-            print("You choose to\033[1;31m look\033[m at what emerges.\n")
-            user_input = "valid"
-            sarcophagus_look()
-        elif choice2 == "escape":
-            time.sleep(1)
-            print("You choose to\033[1;34m escape\033[m.\n")
-            user_input = "valid"
-            sarcophagus_escape()
-        else:
-            print("You hear your name on the breeze.\nPlease choose to\033[1;31m look\033[m or \033[1;34m escape\033[m.\n")
+    choice = Choice("look", "escape")
+    user_input = question_template(choice)
+    if user_input == choice.first_choice:
+        look()
+    else:
+        escape()
 
 
-def sarcophagus_look():
+def look():
     """
     Reads the dungeon script to look at what
     emerges.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [90, 91, 92, 93, 94, 95]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [97, 98]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [100, 101, 102, 103, 104]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [106, 107, 108, 109]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [111, 112, 113]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [115]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    game_over()
+    lines = [90, 91, 92, 93, 94, 95]
+    read_script(lines)
+    time.sleep(1)
+    lines = [97, 98]
+    read_script(lines)
+    time.sleep(1)
+    lines = [100, 101, 102, 103, 104]
+    read_script(lines)
+    time.sleep(1)
+    lines = [106, 107, 108, 109]
+    read_script(lines)
+    time.sleep(1)
+    lines = [111, 112, 113]
+    read_script(lines)
+    time.sleep(1)
+    lines = [115]
+    read_script(lines)
+    time.sleep(1)
+    Life.reduce_life()
+    print("Do you give in to temptation and\033[1;34m look\033[m\n")
+    print("or do you\033[1;35m escape\033[m?\n")
+    scene5_choice()
 
 
-def sarcophagus_escape():
+def escape():
     """
     Reads the dungeon script to escape.
     """
-    file = open("dungeon.txt", encoding="utf8")
-    read_lines = [118, 119, 120, 121]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [123, 124]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [126, 127, 128, 129, 130]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [132]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    time.sleep(2)
-    read_lines = [134, 135, 136, 137]
-    for i, spec_line in enumerate(file):
-        if i in read_lines:
-            print(spec_line)
-    file.close()
-    time.sleep(2)
-    win_game()  
+    lines = [118, 119, 120, 121]
+    read_script(lines)
+    time.sleep(1)
+    lines = [123, 124]
+    read_script(lines)
+    time.sleep(1)
+    lines = [126, 127, 128, 129, 130]
+    read_script(lines)
+    time.sleep(1)
+    lines = [132]
+    read_script(lines)
+    time.sleep(1)
+    lines = [134, 135, 136, 137]
+    read_script(lines)
+    time.sleep(1)
+    win_game()
 
 
 def game_over():
@@ -391,13 +347,15 @@ def game_over():
         restart_game = input("Enter y/n: \n")
         if restart_game == "y":
             user_input = "valid"
-            initialize_game()
+            Life.life = 3
+            main()
         elif restart_game == "n":
             print("Until next time...")
             user_input = "valid"
             break
         else:
-            print("You feel the chill of icy breath at your back.\nPlease type y or n")
+            print("You feel the chill of icy breath at your back.\n")
+            print("Please type y or n\n")
 
 
 def win_game():
@@ -411,24 +369,26 @@ def win_game():
     user_input = "invalid"
     while user_input == "invalid":
         time.sleep(1)
-        restart_game = input("Enter y/n: \n")
+        restart_game = input("Care to play again? Enter y/n: \n")
         if restart_game == "y":
             user_input = "valid"
-            initialize_game()
+            Life.life = 3
+            main()
         elif restart_game == "n":
             print("Until next time...")
             user_input = "valid"
             break
         else:
-            print("You feel the chill of icy breath at your back.\nPlease type y or n")
- 
+            print("Your bed creaks under the weight of your hoard.\n")
+            print("Please type y or n\n")
+
 
 def main():
     """
     Run program functions
     """
     game_logo()
-    initialize_game()
+    scene1()
 
 
 main()
